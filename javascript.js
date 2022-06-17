517299741345af09c7aa0afaa43bb39267eecc88
Zikasd <markozivkovic82@yahoo.com>
2022-06-17 13:03:44 +0200

    computerPlay random selecton done

--- javascript.js
+++ javascript.js
@@ -1,1 +1,11 @@
-console.log("Hello World");
+function computerPlay(arr) {
+    const randomIndex = Math.floor(Math.random() * arr.length);
+
+    const item = arr[randomIndex];
+
+    return item;
+}
+
+const array = ['rock', 'paper', 'scissors'];
+
+
