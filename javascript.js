4229c8fa69991e42f5de857df735464f7b7a87a2
Zikasd <markozivkovic82@yahoo.com>
2022-06-20 15:05:35 +0200

    Define single round of game

--- javascript.js
+++ javascript.js
@@ -6,6 +6,29 @@ function computerPlay() {
     return computerOptins[randomIndex];
 }
 
+function playRound(playerSelection, computerSelection) {
+    if (playerSelection === computerSelection) {
+        return 'Play again';
+    } else if (playerSelection == 'rock' && computerSelection == 'paper') {
+        return 'You Lose! Paper beats Rock';
+    } else if (playerSelection == 'paper' && computerSelection == 'scissors') {
+        return 'You Lose! Scissors beats Paper';
+    } else if (playerSelection == 'scissors' && computerSelection == 'rock') {
+        return 'You Lose! Rock beats Scissors';
+    } else if (playerSelection == 'paper' && computerSelection == 'rock') {
+        return 'You Win! Paper beats Rock';
+    } else if (playerSelection == 'scissors' && computerSelection == 'paper') {
+        return 'You Win! Scissors beats Paper';
+    } else if (playerSelection == 'rock' && computerSelection == 'scissors') {
+        return 'You Win! Rock beats Scissors';
+    } else {
+        return 'Bad input';
+    }
+        
+  }
+  
+  const playerSelection = 'rock';
+  const computerSelection = computerPlay();
+  console.log(playRound(playerSelection, computerSelection));
 
 
-console.log(computerPlay());
